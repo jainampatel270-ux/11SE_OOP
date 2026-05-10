33ab27e5c19a1ac45bdc08580b1278e97e3af151
jainampatel270-ux <jainampatel270@gmail.com>
2026-05-10 11:11:03 +0000

    Activity 3

--- OOP_Exercises/1c. pet_lists.py
+++ OOP_Exercises/1c. pet_lists.py
@@ -7,6 +7,24 @@ Species: Dog
 Age: 8
 Vaccination Status: False
 '''
+#1 - Parallel List
+pet_name = ['Foxy', 'Max', 'Bob']
+species = ['Dog', 'Cat', 'Fish']
+age = [4, 7, 2]
+vaccination_status = [False, True, False]
+
+
+
+#2 Print Parallel List
+for i in range(len(pet_name)):
+    print('Pet Name:', pet_name[i])
+    print('Species:', species[i])
+    print('Age:', age[i])
+    print('Vaccination Status:', vaccination_status[i])
+    print()
+
+
+
 #3. Demonstrate what happens when an item is deleted
 
 
@@ -14,7 +32,34 @@ Vaccination Status: False
 
   #ACTIVITIES:
 # In each activity test out that the printing of data is still valid
-
 #1. Add a new animal named Hootie, its a blowfish, it is 34 years
 #2. Vaccinate an unvaccinated animal (create vaccination)
 #3. Remove an animal and make sure that all the printing is correct
+
+
+
+#1. Add a new animal named Hootie, its a blowfish, it is 34 years
+pet_name = ['Foxy', 'Max', 'Bob', 'Hootie']
+species = ['Dog', 'Cat', 'Fish', 'Blowfish']
+age = [4, 7, 2, 34]
+vaccination_status = [False, True, False, False]
+
+
+
+#2. Vaccinate an unvaccinated animal (create vaccination)
+vaccination_status[2] = True
+
+
+
+#3. Remove an animal and make sure that all the printing is correct
+del pet_name[1]
+del species[1]
+del age[1]
+del vaccination_status[1]
+
+for i in range(len(pet_name)):
+    print('Pet Name:', pet_name[i])
+    print('Species:', species[i])
+    print('Age:', age[i])
+    print('Vaccination Status:', vaccination_status[i])
+    print()
